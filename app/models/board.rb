6ad5eb529d7306a81f45564778f3e201f9ac26e1
Erwin Rodriguez <erwin@kitfoxdesign.com>
2023-01-13 01:08:02 -0800

    homepage updates and tweaked board grid generator to fix board filling up bug

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,9 @@
 class Board < ApplicationRecord
+  validates :width, presence: true, numericality: { greater_than: 0 }
+  validates :height, presence: true, numericality: { greater_than: 0 }
+  validates :mine_count, presence: true, numericality: { greater_than_or_equal_to: 0, less_than: :area}
+
+  def area 
+    return self.width * self.height
+  end
 end
